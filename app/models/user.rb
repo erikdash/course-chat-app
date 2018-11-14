b490f7d46e5366819074f5a038f74f596964e26a
ldtaylor <lut11@pitt.edu>
2018-11-14 16:00:11 -0500

    user nickname and permissions set

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,26 @@ class User < ApplicationRecord
     self.email_confirmed_at = Time.current
     save
   end
+
+  def set_nickname(nickname)
+    self.nickname = nickname
+  end
+
+  def get_nickname
+    return self.nickname
+  end
+
+  def get_starred_chatrooms
+    return self.chatrooms
+  end
+
+  def set_starred_chatrooms chatroom
+    self.chatrooms << chatroom
+  end
+
+  def remove_starred_chatroom chatroom_name
+
+
+  end
+
 end
