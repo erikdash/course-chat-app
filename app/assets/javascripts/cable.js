5fbaf82bc4a5a82877b6abc7d27304553ce4c90c
Josh961 <jlz37@pitt.edu>
2018-12-03 17:36:34 -0500

    create consumer

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -8,6 +8,6 @@
 (function() {
   this.App || (this.App = {});
 
-  App.cable = ActionCable.createConsumer();
+  App.cable = ActionCable.createConsumer("ws://mighty-coast-82167.herokuapp.com");
 
 }).call(this);
